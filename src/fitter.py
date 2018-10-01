b338ab2303ebe39c5f93f8d0e14deb4ee43dc18d
michaelschf1 <michaelschf1@gmail.com>
2018-10-02 01:18:38 +1000

    v3

--- src/fitter.py
+++ src/fitter.py
@@ -7,38 +7,47 @@ Created on 1 Oct. 2018
 '''
 
 from resonator_tools import circuit
-import numpy as np
-import pandas as pd
 from utilities import readCSV, trim, calibrate_S21dB, writeTXT
 
-
-folder = '/Users/mykhailo/OneDrive - UNSW/research/measurements/Sample PBG AA1/LGR_PBG_28Aug18_1.4K_meas_NoCal/'
+# Data file path
+folder = ("/Users/mykhailo/OneDrive - UNSW/research/measurements/" +
+          "Sample PBG AA1/LGR_PBG_28Aug18_1.4K_meas_NoCal/")
 file_s21 = folder + 'SMA1-3_peak7.8_noDC_n20.csv'
 
-folder_cal = '/Users/mykhailo/OneDrive - UNSW/research/measurements/Sample PBG AA1/LGR_PBG_25Aug18_calibration_1.4K/'
+# Calibration file path
+folder_cal = ("/Users/mykhailo/OneDrive - UNSW/research/measurements/" +
+              "Sample PBG AA1/LGR_PBG_25Aug18_calibration_1.4K/")
 file_cal = folder_cal + 'S21_SMA1t0SMA2_1p4K_0dBm.csv'
 
+# Frequency range that will be analyzed
 fmin = 7.83  # frequency in [GHz]
 fmax = 7.88  # frequency in [GHz]
 
+# Reading csv file
 data = readCSV(file_s21, headers=7)
+# Cut according to the chosen frequency range
 data = trim(data, fmin, fmax)
+# Add calibration if needed
 data = calibrate_S21dB(data, file_cal)
-
+# Writing the data to txt file for further fitting
 writeTXT('S21.txt', data)
-# 
-# port1 = circuit.notch_port()
-# port1.add_fromtxt('S21.txt', 'dBmagphasedeg', 1)
+
+# Using library "resonator_tools" for fitting notch type resonators
+port1 = circuit.notch_port()
+port1.add_fromtxt('S21.txt', 'dBmagphasedeg', 1)
 print('reading of the data is succesfully finished')
 
-# port1.autofit()
-# print('autofit is done')
-# print("Fit results:", port1.fitresults)
-# port1.plotall()
+port1.autofit(electric_delay=None)
+print('autofit is done')
+print("Fit results:", port1.fitresults)
+port1.plotall()
 
 # port1.GUIfit()
 # print("Fit results:", port1.fitresults)
 # port1.plotall()
-# print("single photon limit:", port1.get_single_photon_limit(diacorr=True), "dBm")
-# print("photons in reso for input -140dBm:", port1.get_photons_in_resonator(-140,unit='dBm',diacorr=True), "photons")
-# print("done")
+# print("single photon limit:",
+#       port1.get_single_photon_limit(diacorr=True), "dBm")
+# print("photons in reso for input -140dBm:",
+#       port1.get_photons_in_resonator(-140, unit='dBm', diacorr=True),
+#       "photons")
+print("done")
--- src/utilities.py
+++ src/utilities.py
@@ -9,6 +9,7 @@ from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
 
+# cut the data according to set frequency range
 def trim(data, fmin, fmax):
     data_new = []
     fmin = fmin * 1e9  # conversion back to [Hz]
@@ -22,6 +23,7 @@ def trim(data, fmin, fmax):
     return data_new
 
 
+# read a csv file to buffer
 def readCSV(file, headers=7):
     with open(file) as csvfile:
         reader = csv.reader(csvfile)
@@ -36,21 +38,25 @@ def readCSV(file, headers=7):
     return data
 
 
+# write to txt file formatted data
 def writeTXT(file, data):
     f = open(file, 'w')
     for line in data:
         if len(line) > 2:
-            f.write(str(line[0]) + "\t")
-            f.write(str(line[1]) + "\t")
-            f.write(str(line[2]) + "\n")
+            line = line.split(" ")
+            f.write(str(line[0]) + ' ')
+            f.write(str(line[1]) + ' ')
+            f.write(str(line[2]) + '\n')
     f.close()
     print('writing to txt file is done')
 
 
+# exception when the frequency range is chosen outside the available data
 class NotEnoughInputData(Exception):
     pass
 
 
+# do calibaration for dB magnitude S21
 def calibrate_S21dB(data, file_cal):
     data_cal = readCSV(file_cal, headers=7)
     S21dB_cal_func = interpolate_S21dB(data_cal)
@@ -61,21 +67,23 @@ def calibrate_S21dB(data, file_cal):
     return data_calibrated
 
 
+# form one data array from three lists
 def format_data(f, S21, phase):
     data = []
     for i in range(len(f)):
-        print(f[i])
         line = str(f[i]) + ' ' + str(S21[i]) + ' ' + str(phase[i]) + '\n'
         data.append(line)
     return data
 
 
+# interpolation of calibration points for its further implementation
 def interpolate_S21dB(data):
     freq_Hz, S21_dB, phase = format_S21(data)  # @UnusedVariable
     S21_dB_interpol = interp1d(freq_Hz, S21_dB, kind='cubic')
     return S21_dB_interpol
 
 
+# form three lists form one array
 def format_S21(data):
     freq_Hz = []
     S21_dB = []
@@ -88,12 +96,14 @@ def format_S21(data):
     return freq_Hz, S21_dB, phDeg
 
 
+# plot two data lists
 def plot_pair(x1, y1, x2, y2):
     plt.plot(x1, y1, 'o', x2, y2, '-')
     plt.legend(['data', 'cubic'], loc='best')
     plt.show()
 
 
+# plot one data list
 def plot_single(x1, y1):
     plt.plot(x1, y1, 'o')
     plt.legend(['data'], loc='best')
