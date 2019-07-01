51945fbb1ac0b4e1f424b06dbb6732ceaab476ee
michaelschf1 <32321209+michaelschf1@users.noreply.github.com>
2019-07-01 22:01:41 +1000

    update

--- src/fitAll.py
+++ src/fitAll.py
@@ -16,12 +16,13 @@ import re, os
 
 # Data file path
 #folder  = '/Users/mykhailo/OneDrive - UNSW/research/measurements/ICE He3/BA11' #for Mac
-folder = 'C:/Users/z5119993/OneDrive - UNSW/research/measurements/ICE He3/BA11_bondWiresReson/PBG/500mK/power' #for Windows
+folder = 'C:/Users/z5119993/OneDrive - UNSW/research/measurements/ICE He3/A9nm_CA1/LGR_tuned_27062019/PBG_Vdc_sweep' #for Windows
 # file = 'BA12_PBG_400MK_N40_50DBATTCOM.csv'
 # file_csv = folder + '/' + file
 
 
-regex = "BA11_PBG_500mK_n(\d*)_V0.csv"
+regex = "CA1_PBG_n10_60dBAtten_490mK_(\d*\.\d*)V.csv"
+# regex = "CA1_PBG_n10_60dBAtten_490mK_(\d*)V.csv"
 
 for fname in os.listdir(folder):
     m = re.match(regex, fname)
@@ -30,19 +31,27 @@ for fname in os.listdir(folder):
         file_csv = os.path.join(folder, m.string)
 
         df = pd.read_csv(file_csv)
-        df_transposed = df.transpose()
-        df = df_transposed.iloc[1:, ]
-        display(df)
-        port1 = circuit.reflection_port(f_data=df[0].values,
-                                        z_data_raw=10 ** (df[1].values / 20) * np.exp(
-                                            1j * df[2].values / 180 * np.pi))
+
+        mags = []
+        for c in df["S21 complex"]:
+            mags.append(complex(c))
+
+        port1 = circuit.reflection_port()
+        port1.add_data(df["Frequency, Hz"], mags)
+
+        # df_transposed = df.transpose()
+        # df = df_transposed.iloc[1:, ]
+        # display(df)
+        # port1 = circuit.reflection_port(f_data=df[0].values,
+        #                                 z_data_raw=10 ** (df[1].values / 20) * np.exp(
+        #                                     1j * df[2].values / 180 * np.pi))
 
         # fit the corected data
         port1.GUIfit()
         df_results = pd.DataFrame([port1.fitresults])
-        df_results['Source power, dBm'] = voltage
+        df_results['VoltageDC, V'] = voltage
         display(df_results)
         ut.append_df_to_excel(
-            'C:/Users/z5119993/OneDrive - UNSW/research/measurements/ICE He3/BA11_bondWiresReson/BA11_PBG_power_500mK_FITS.xlsx',
+            'C:/Users/z5119993/OneDrive - UNSW/research/measurements/ICE He3/A9nm_CA1/RESULTS/PBG_CA1_S2_Vdc_FITS.xlsx',
             df_results)
 
--- src/fitter.py
+++ src/fitter.py
@@ -13,8 +13,8 @@ import pandas as pd
 
 # Data file path
 #folder  = '/Users/mykhailo/OneDrive - UNSW/research/measurements/ICE He3/BA11' #for Mac
-folder = 'C:/Users/z5119993/OneDrive - UNSW/research/measurements/ICE He3/BA11' #for Windows
-file = 'BA11_PBG_500mK_n10_V14.05.csv'
+folder = 'C:/Users/z5119993/OneDrive - UNSW/research/measurements/ICE He3/A9nm_CA1' #for Windows
+file = 'CA1_LGR_RT_N15_7_COM.csv'
 
 
 file_csv = folder + '/' + file
@@ -27,20 +27,24 @@ ut.convertCSVtoTXT(file_csv, file_txt)
 # Using library "resonator_tools" for fitting notch type resonators
 port1 = circuit.reflection_port()
 
-port1.add_fromtxt(file_txt, 'dBmagphasedeg', 1, delimiter=' ')
+port1.add_fromtxt(file_txt, 'dBmagphasedeg', 18, delimiter=' ')
 print('reading of the data is successfully finished')
 #port1.add_fromtxt(file_csv,'linmagphasedeg',3)
 
 
-port1.autofit(electric_delay=None)
-print('autofit is done')
+# port1.autofit(electric_delay=None)
+# # port1.GUIbaselinefit()
+# print('autofit is done')
+# print("Fit results:", port1.fitresults)
+# port1.GUIbaselinefit()
+
+# fit the corrected data
+port1.GUIfit()
 print("Fit results:", port1.fitresults)
 port1.plotall()
 
-port1.GUIfit()
-
-print("single photon limit:",
-      port1.get_single_photon_limit(diacorr=True), "dBm")
+# print("single photon limit:",
+#       port1.get_single_photon_limit(diacorr=True), "dBm")
 # print("photons in reso for input -140dBm:",
 #       port1.get_photons_in_resonator(-140, unit='dBm', diacorr=True),
 #       "photons")
