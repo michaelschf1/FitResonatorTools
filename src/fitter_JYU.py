dbaec14713e76ec3f4483299ffeb9cd074d85051
michaelschf1 <32321209+michaelschf1@users.noreply.github.com>
2019-10-03 17:54:04 +1000

    Update fitter_JYU.py

--- src/fitter_JYU.py
+++ src/fitter_JYU.py
@@ -18,9 +18,13 @@ file_mag = 'dat-short-magn.txt'
 file_phase = 'dat-short-phase.txt'
 file_freq = 'freqs-antenna2.txt'
 
+
+file = folder + '/' + file_mag.replace('.txt', '') + '_COM.txt'
 file_mag = folder + '/' + file_mag
 file_phase = folder + '/' + file_phase
 file_freq = folder + '/' + file_freq
+
+
 
 with open(file_mag, "r") as my_input_file:
     data_mag = my_input_file.read()
@@ -41,29 +45,43 @@ print('conversion successful!')
 print(len(mags))
 print(len(phases))
 print(len(freqs))
-#
-# # Using library "resonator_tools" for fitting notch type resonators
-# port1 = circuit.reflection_port()
-#
-# port1.add_fromtxt(file_txt, 'dBmagphasedeg', 18, delimiter=' ')
-# print('reading of the data is successfully finished')
-# #port1.add_fromtxt(file_csv,'linmagphasedeg',3)
-#
-#
-# # port1.autofit(electric_delay=None)
-# # # port1.GUIbaselinefit()
-# # print('autofit is done')
-# # print("Fit results:", port1.fitresults)
+
+with open(file, "w") as my_out_file:
+    my_out_file.write('Frequency ')
+    my_out_file.write('Magnitude ')
+    my_out_file.write('Phase' + '\n')
+
+    for i in range(len(mags)):
+        if mags[i] !='':
+            my_out_file.write(freqs[i] + ' ')
+            my_out_file.write(mags[i] + ' ')
+            my_out_file.write(phases[i] + '\n')
+
+    my_input_file.close()
+
+
+# Using library "resonator_tools" for fitting notch type resonators
+port1 = circuit.reflection_port()
+
+port1.add_fromtxt(file, 'linmagphasedeg', 1, delimiter=' ')
+print('reading of the data is successfully finished')
+#port1.add_fromtxt(file_csv,'linmagphasedeg',3)
+
+
+# port1.autofit(electric_delay=None)
 # # port1.GUIbaselinefit()
-#
-# # fit the corrected data
-# port1.GUIfit()
+# print('autofit is done')
 # print("Fit results:", port1.fitresults)
-# port1.plotall()
-#
-# # print("single photon limit:",
-# #       port1.get_single_photon_limit(diacorr=True), "dBm")
-# # print("photons in reso for input -140dBm:",
-# #       port1.get_photons_in_resonator(-140, unit='dBm', diacorr=True),
-# #       "photons")
-#
+# port1.GUIbaselinefit()
+
+# fit the corrected data
+port1.GUIfit()
+print("Fit results:", port1.fitresults)
+port1.plotall()
+
+# print("single photon limit:",
+#       port1.get_single_photon_limit(diacorr=True), "dBm")
+# print("photons in reso for input -140dBm:",
+#       port1.get_photons_in_resonator(-140, unit='dBm', diacorr=True),
+#       "photons")
+
